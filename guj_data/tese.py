a6d6f59efb7aab3c760f292ec71cd0a47aaba744
I RUN MAN <39081953+jatinupadhyay@users.noreply.github.com>
2018-05-08 13:56:08 +0530

    Add files via upload

---
+++ guj_data/tese.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import subprocess
+cmd ="cd"
+os.system(cmd)
+cmd= "sudo apt install tesseract-ocr"
+os.system(cmd)
+cmd = "sudo apt install libtesseract-dev"
+os.system(cmd)
+cmd = "cd /usr/share/tesseract-ocr/4.00"
+os.system(cmd)
+cmd = "firefox https://github.com/tesseract-ocr/tessdata/blob/master/guj.traineddata"
+os.system(cmd)
+
+cmd = "tesseract 1.png 1_out -l guj"
+os.system(cmd)
+
+cmd = "tesseract 2.png 1_out -l guj"
+os.system(cmd)
+
+cmd = "tesseract 3.png 1_out -l guj"
+os.system(cmd)
+
